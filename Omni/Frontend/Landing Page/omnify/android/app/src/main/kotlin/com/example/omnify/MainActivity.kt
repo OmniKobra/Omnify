09b03da32b69f58959ad1f15bd4842ec8ebd5a8c
OmniKobra <omnifyofficial@yahoo.com>
2024-10-27 16:16:01 +0200

    Delete Omni directory

--- Omni/Frontend/Landing Page/omnify/android/app/src/main/kotlin/com/example/omnify/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.omnify
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- Omni/Frontend/Landing Page/omnify/lib/models/swiper_item.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class SwiperItem {
-  final String title;
-  final List<String> features;
-  final Color color;
-  final IconData icon;
-  const SwiperItem(this.title, this.features, this.color, this.icon);
-}
